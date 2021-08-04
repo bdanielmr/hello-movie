fe253bb9cef0b25c65dce116876e72ec9cfb2817
danielmr <bdanielmr@gmail.com>
2021-08-04 03:49:25 -0500

    add qpagination router adding params and page querys

--- src/views/HomeSearchMovie/HomeSearchMovie.jsx
+++ src/views/HomeSearchMovie/HomeSearchMovie.jsx
@@ -1,14 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import SearchMovie from '../../components/SearchMovie/SearchMovie';
 import CustomTopList from '../../components/CustomTopList/CustomTopList';
 import style from './homeSearchMovie.module.scss';
 import CustomPagination from '../../components/CustomPagination/CustomPagination';
 import { StoreContext } from '../../store/StoreProvider';
+import { useHistory } from 'react-router-dom';
 const HomeSearchMovie = (props) => {
   const [store] = useContext(StoreContext);
   const { listMovies, paginationList } = store;
+  const [query, setQuery] = useState(paginationList);
+  const history = useHistory();
+  console.log('h', history);
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+    if (paginationList) {
+      params.append('page', paginationList?.page);
+    } else {
+      params.delete('page');
+    }
+    history.push({ search: params.toString() });
+  }, [paginationList, history]);
+
   console.log('ver list movies', paginationList);
   return (
     <>
